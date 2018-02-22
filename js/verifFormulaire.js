7b769ee4faa6731ae3abc73c9069344c36fdf6a0
Pecpec <36603040+Natelou@users.noreply.github.com>
2018-02-22 22:54:58 +0100

    Add files via upload

--- js/verifFormulaire.js
+++ js/verifFormulaire.js
@@ -9,16 +9,24 @@ function surligne(champ, erreur)
 function verifNom() {
 	var champ = document.getElementById('nom');
 	champ.addEventListener('blur', function() {
-		if(champ.value.length < 1 || champ.value.length > 25)
-		{
-			alert("Champ nom mal renseigné : champ vide ou taille supérieure à 25 caractères");
+		if (champ.value.length==0 || champ.value.replace(/\s/g,"").length==0){
+			alert("Champ nom non renseigné");
 			surligne(champ, true);
 			return false;
 		}
-		else
+		else 
 		{
-			surligne(champ, false);
-			return true;
+			if(champ.value.length < 1 || champ.value.length > 25)
+			{
+				alert("Champ nom mal renseigné : champ vide ou taille supérieure à 25 caractères");
+				surligne(champ, true);
+				return false;
+			}
+			else
+			{
+				surligne(champ, false);
+				return true;
+			}
 		}
 	});	
 }
@@ -26,16 +34,24 @@ function verifNom() {
 function verifPrenom() {
 	var champ = document.getElementById('prenom');
 	champ.addEventListener('blur', function() {
-		if(champ.value.length < 1 || champ.value.length > 25)
-		{
-			alert("Champ prénom mal renseigné : champ vide ou taille supérieure à 25 caractères");
-			surligne(champ, true);
-			return false;
+		if (champ.value.length==0 || champ.value.replace(/\s/g,"").length==0){
+		alert("Champ prénom non renseigné");
+		surligne(champ, true);
+		return false;
 		}
-		else
+		else 
 		{
-			surligne(champ, false);
-			return true;
+			if(champ.value.length < 1 || champ.value.length > 25)
+			{
+				alert("Champ prénom mal renseigné : champ vide ou taille supérieure à 25 caractères");
+				surligne(champ, true);
+				return false;
+			}
+			else
+			{
+				surligne(champ, false);
+				return true;
+			}
 		}
 	});	
 }
@@ -94,16 +110,24 @@ function verifTelephone() {
 function verifLogin() {
 	var champ = document.getElementById('login');
 	champ.addEventListener('blur', function() {
-		if(champ.value.length < 1)
-		{
+		if (champ.value.length==0 || champ.value.replace(/\s/g,"").length==0){
 			alert("Champ login non renseigné");
 			surligne(champ, true);
 			return false;
 		}
-		else
+		else 
 		{
-			surligne(champ, false);
-			return true;
+			if(champ.value.length < 1)
+			{
+				alert("Champ login non renseigné");
+				surligne(champ, true);
+				return false;
+			}
+			else
+			{
+				surligne(champ, false);
+				return true;
+			}
 		}
 	});	
 }
@@ -165,16 +189,24 @@ function verifPassword() {
 function verifLibelle() {
 	var champ = document.getElementById('libelle');
 	champ.addEventListener('blur', function() {
-		if(champ.value.length < 1 || champ.value.length > 255)
-		{
-			alert("Champ libellé mal renseigné : champ vide ou taille supérieure à 255 caractères");
+		if (champ.value.length==0 || champ.value.replace(/\s/g,"").length==0){
+			alert("Champ libellé non renseigné");
 			surligne(champ, true);
 			return false;
 		}
-		else
+		else 
 		{
-			surligne(champ, false);
-			return true;
+			if(champ.value.length < 1 || champ.value.length > 255)
+			{
+				alert("Champ libellé mal renseigné : champ vide ou taille supérieure à 255 caractères");
+				surligne(champ, true);
+				return false;
+			}
+			else
+			{
+				surligne(champ, false);
+				return true;
+			}
 		}
 	});
 }
